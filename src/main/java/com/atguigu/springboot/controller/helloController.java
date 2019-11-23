e969ec7c76ec03c2c0c9b62a11924b0088a4cf54
libingbing <libingbing@huimi.com>
2019-11-23 15:51:11 +0800

    修改了该文件

--- src/main/java/com/atguigu/springboot/controller/helloController.java
+++ src/main/java/com/atguigu/springboot/controller/helloController.java
@@ -3,5 +3,6 @@ package com.atguigu.springboot.controller;
 public class helloController {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("ee");
     }
 }
